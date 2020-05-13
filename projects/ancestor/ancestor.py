f30190f5f88bde38ffa0ab2b95130b48af4c8964
Anthony Hill <hillan1152@gmail.com>
2020-05-12 21:13:13 -0400

    just starting graph problem

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,20 @@
+from graph import Graph
+from util import Queue
 
 def earliest_ancestor(ancestors, starting_node):
+    g = Graph()
+    for ancestor in ancestors:
+        g.add_vertex(ancestor[0])
+        g.add_vertex(ancestor[1])
+        g.add_edge(ancestor[1], ancestor[0])
+    print(g.dft(starting_node))
+
+        
+    # print(g.get_neighbors(8))
+
+
     pass
+
+if __name__ == '__main__':
+    test_ancestors = [(1, 3), (2, 3), (3, 6), (5, 6), (5, 7), (4, 5), (4, 8), (8, 9), (11, 8), (10, 1)]
+    earliest_ancestor(test_ancestors, 3)
